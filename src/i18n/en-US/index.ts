03ba8d3b096c9187acefe5355368345740b629ce
Michael G <me@mgineer85.de>
2025-05-10 15:20:46 +0200

    add languages

--- src/i18n/en-US/index.ts
+++
@@ -1,8 +0,0 @@
-export default {
-  'url invalid': 'url invalid!',
-  'For security reasons, the requested URL needs to be on the same host!':
-    'For security reasons, the requested URL needs to be on the same host!',
-  'The requested URL "{requestedUrl}" must be relative or absolute but same hostname as this page "{requiredUrl}".':
-    'The requested URL "{requestedUrl}" must be relative or absolute but same hostname as this page "{requiredUrl}".',
-  "Save or share what you've captured": "Save or share what you've captured",
-};
